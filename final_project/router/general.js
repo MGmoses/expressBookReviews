37d46625193d6fe045d5cc8de3f88d629227e3eb
MGmoses <ichibanthah@gmail.com>
2026-06-13 07:40:50 -0400

    reviews pull

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,19 +27,28 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const matchingBooks = Object.keys(books)
+  .filter(isbn => books[isbn].author === author)
+  .map(isbn => books[isbn]);
+  return res.send(JSON.stringify(matchingBooks, null, 4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = Object.keys(books)
+  .filter(isbn => books[isbn].title === title)
+  .map(isbn => books[isbn]);
+  return res.send(JSON.stringify(matchingBooks, null, 4));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn].reviews, null, 4));
 });
 
 module.exports.general = public_users;

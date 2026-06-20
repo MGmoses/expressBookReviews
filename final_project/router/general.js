f1fe7106c1c487e53e9ecbf07424de96d442d3d3
MGmoses <ichibanthah@gmail.com>
2026-06-20 01:21:54 -0400

    promise book fetch

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,15 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books, null, 4));
+  new Promise((resolve, reject) => {
+    resolve(books);
+  })
+  .then((books) => {
+    return res.status(200).json(JSON.stringify(books, null, 4));
+  })
+  .catch((err) => {
+    return res.status(500).json({message: "Error fetching books"});
+  });
 });
 
 // Get book details based on ISBN

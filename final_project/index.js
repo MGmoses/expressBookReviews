490e2eee1d81bf4d6e8d4958537785a8134402f5
MGmoses <ichibanthah@gmail.com>
2026-06-22 00:16:13 -0400

    update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const gen1_routes = require('./router/general.js').general;
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
+app.use("/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken'];
@@ -31,7 +31,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
  
 const PORT =5000;
 
-app.use("/customer", customer_routes);
-app.use("/", genl_routes);
+app.use("/", customer_routes);
+app.use("/", gen1_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
